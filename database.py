a85dc63b7255c40bfb660f18899b508c234513fc
= <mauricio.a.almeida@gmail.com>
2025-07-02 16:36:48 -0300

    adicionado suporte ao postgres

--- database.py
+++ database.py
@@ -2,14 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 # Caminho para o arquivo SQLite (pode ser ajustado conforme necessário)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'escola.db')}"
-
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'escola.db')}"
+engine = create_engine(DATABASE_URL)
+#engine = create_engine(
+#    DATABASE_URL, connect_args={"check_same_thread": False}
+#)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
